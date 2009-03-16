1bea55b06707728b844fdf8fa7df858aa32ef66d
Keiji, Yoshimi <walf443@gmail.com>
2009-03-16 22:57:17 +0900

    作成日、更新日を追加

--- lib/todoit/model/task.rb
+++ lib/todoit/model/task.rb
@@ -1,4 +1,6 @@
 # vim: encoding=utf-8
+require 'time'
+require 'rubygems'
 require 'classx'
 
 module Todoit
@@ -7,13 +9,22 @@ module Todoit
       include ClassX
       include ClassX::Bracketable
 
+      time_spec = {
+        :kind_of => Time,
+        :coerce => {
+          String => proc {|val| Time.parse(val) },
+        }
+      }
+
       has :title
+      has :created_at, time_spec
+      has :updated_at, time_spec
 
       class << self
         def get
           [
-            { :title => "Todoitを作る" },
-            { :title => "Modelを作る" },
+            { :title => "Todoitを作る" , :created_at => '2009-03-01 10:00:00', :updated_at => '2009-03-16 22:40' },
+            { :title => "Modelを作る", :created_at  => '2009-03-05 10:00:00', :updated_at => '2009-03-16 22:40' },
           ].map {|i| self.new(i) }
         end
       end
