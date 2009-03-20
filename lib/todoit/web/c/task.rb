d8f71f06741dbe970eb0db5151c32d76ca4deab1
Keiji, Yoshimi <walf443@gmail.com>
2009-03-20 18:33:58 +0900

    added title.

--- lib/todoit/web/c/task.rb
+++ lib/todoit/web/c/task.rb
@@ -11,6 +11,7 @@ module Todoit
         def on_index
           tasks = Todoit::Model::Task.get
           context.view.render('assets/template/layout.erb.html', {
+            :title => "タスク一覧",
             :main_template => 'assets/template/task/index.erb.html',
             :tasks => tasks ,
           })
