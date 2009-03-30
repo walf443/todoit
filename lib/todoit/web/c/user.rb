3bbdd79c0a0a0fbe12fe957dc2dd218f8d9564f6
Keiji, Yoshimi <walf443@gmail.com>
2009-03-30 22:42:45 +0900

    仮でとりあえず追加だけしておく

---
+++ lib/todoit/web/c/user.rb
@@ -0,0 +1,23 @@
+module Todoit
+  module Web
+    module C
+      module User
+        extend FunctionImporter
+
+        module_function
+
+        def on_index
+        end
+
+        def on_add
+        end
+
+        def on_edit
+        end
+
+        def on_delete
+        end
+      end
+    end
+  end
+end
